61748b3f6bfa7194dd24822f4347136b8e611842
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-07-26 15:58:58 -0400

    shifted to class structure - implementing skeleton

--- bitcoin_networks/miners.py
+++ bitcoin_networks/miners.py
@@ -15,32 +15,59 @@ import pandas as pd
 
 
 #%%
-def bitnodes(url='https://bitnodes.earn.com/api/v1/snapshots/latest/'):
-    headers = {'Accept': 'application/json; indent=4', }
-    bitnodes_data = requests.get(url,
-                                 headers=headers
-                                 )
-    bitnodes = pd.read_json(bitnodes_data.text)
-    node_fields = ['Protocol_version',
-                   'User_agent',
-                   'Connected_since',
-                   'Services',
-                   'Height',
-                   'Hostname',
-                   'City',
-                   'Country_code',
-                   'LAT',
-                   'LON',
-                   'Timezone',
-                   'ASN',
-                   'Organization_name'
-                   ]
-    bitnodes_nodes = pd.DataFrame(bitnodes.nodes.values.tolist(),
-                                  columns=node_fields
-                                  )
-    bitnodes_nodes['Connected_since'] = pd.to_datetime(bitnodes_nodes['Connected_since'],
-                                                       unit='s')
-    bitnodes_nodes.to_csv('data/bitnodes.csv', encoding='utf-8')
+class Bitnodes:
+
+    def bitnodes(url='https://bitnodes.earn.com/api/v1/snapshots/latest/'):
+        """
+        This function will pull the data from bitnodes api for Bitcoin miners.
+        """
+        headers = {'Accept': 'application/json; indent=4', }
+        bitnodes_data = requests.get(url,
+                                     headers=headers
+                                     )
+        bitnodes = pd.read_json(bitnodes_data.text)
+        node_fields = ['Protocol_version',
+                       'User_agent',
+                       'Connected_since',
+                       'Services',
+                       'Height',
+                       'Hostname',
+                       'City',
+                       'Country_code',
+                       'LAT',
+                       'LON',
+                       'Timezone',
+                       'ASN',
+                       'Organization_name'
+                       ]
+        bitnodes_nodes = pd.DataFrame(bitnodes.nodes.values.tolist(),
+                                      columns=node_fields
+                                      )
+        bitnodes_nodes['Connected_since'] = pd.to_datetime(bitnodes_nodes['Connected_since'],
+                                                           unit='s')
+        bitnodes_nodes.to_csv('data/bitnodes.csv', encoding='utf-8')
+
+
+#%%
+class BtcCom:
+
+    def btc_com_nodes(url):
+        """
+        This will pull the node list from btc.com pool
+        Requires an account with access_key and puid
+        url example 'https://pool.api.btc.com/v1/pool/node-list?access_key=${access_key}&puid=${puid}'
+        """
+        btc_com_data = requests.get(url)
+        return btc_com_data
+
+    def btc_com_pool_stats(url):
+        """
+        This will pull the overall mining stats for btc.com pools
+        Requires an account with access_key and puid
+        url example 'https://pool.api.btc.com/v1/blocks?access_key=${access_key}&puid=${puid}'
+        """
+        btc_com_data = requests.get(url)
+        return btc_com_data
 
 
 #%%
