c5ae40191d73c18f589f1dfc2f20465abe51807c
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-08-10 16:39:10 -0400

    github - basic function plan

--- bitcoin_networks/github.py
+++ bitcoin_networks/github.py
@@ -15,6 +15,12 @@ from github import Github
 github_acct = Github()
 
 
+# function for user data
+# function for repo info
+# function for finding blockchain projects
+# function for blockchain repo contributors
+
+
 '''
 if __name__ == "__main__":
     """ This is executed when run from the command line """
