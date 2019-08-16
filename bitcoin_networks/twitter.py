ec0bd21b5643a20f1d256d664de8069053991285
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-08-16 11:01:53 -0400

    twitter - added template

---
+++ bitcoin_networks/twitter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A library for extracting alt-right information from twitter.
+In many case, API keys will be required for access.
+"""
+
+__author__ = "Joe Blankenship"
+__version__ = "19.0.1"
+__license__ = ""
+
+
+'''
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+'''
