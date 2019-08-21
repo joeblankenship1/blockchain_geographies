aac48c7dfb8353b3378b58bbf40288099affbfeb
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-08-21 15:39:26 -0400

    gab - added template

---
+++ bitcoin_networks/gab.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+A library for extracting bitcoin and blockchain developer information.
+In many case, API keys will be required for access.
+"""
+
+__author__ = "Joe Blankenship"
+__version__ = "19.0.1"
+__license__ = ""
+
+
+
+'''
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+'''
