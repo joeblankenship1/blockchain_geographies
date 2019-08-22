206cf3b557c88ea7d506d7bc388a9f504c0b852b
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-08-22 11:06:12 -0400

    distributors - added template

---
+++ bitcoin_networks/distributor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A library for extracting Bitcoin distributor information from several APIs.
+In many case, API keys will be required for access.
+"""
+
+__author__ = "Joe Blankenship"
+__version__ = "19.0.1"
+__license__ = ""
+
+
+'''
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+'''
