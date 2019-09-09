3496c534cc0ef294d03b31d531080164895dc854
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-09-09 00:01:10 -0400

    output - csv function

--- bitcoin_networks/output.py
+++ bitcoin_networks/output.py
@@ -3,6 +3,14 @@
 A library for delivery information in discrete formats.
 """
 
+
+def to_csv(table, output_name):
+    """
+    This function saves data as a CSV file.
+    """
+    table.to_csv(str(output_name), index=False)
+
+
 __author__ = "Joe Blankenship"
 __version__ = "19.0.1"
 __license__ = ""
