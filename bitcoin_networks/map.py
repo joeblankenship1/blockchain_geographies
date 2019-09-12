8f1e1ef4f710ab16b66bdd5a8778e661965fdf80
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-09-11 22:55:19 -0400

    map - added create_cartogram

--- bitcoin_networks/map.py
+++ bitcoin_networks/map.py
@@ -7,7 +7,30 @@ __author__ = "Joe Blankenship"
 __version__ = "19.0.1"
 __license__ = ""
 
+#%%
+import geopandas as gpd
+import geoplot as gplt
+import geoplot.crs as gcrs
 
+
+#%%
+def create_cartogram(filename):
+    """
+    This function produces a cartogram map.
+    """
+    data_raw = gpd.read_file(filename)
+    map = gplt.cartogram(data_raw,
+                         projection=gcrs.Robinson(),
+                         scale=data_raw['NUMPOINTS'],
+                         hue=data_raw['NUMPOINTS'],
+                         cmap='Reds',
+                         k=5,
+                         figsize=(30, 15)
+                         )
+    return map
+
+
+#%%
 '''
 if __name__ == "__main__":
     """ This is executed when run from the command line """
