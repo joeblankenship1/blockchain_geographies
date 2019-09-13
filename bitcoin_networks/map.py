c47c213efa17481ee74ad9ac5c81ed0282f04db9
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-09-13 01:55:48 -0400

    map - added csv import method, modified point to poly method

--- bitcoin_networks/map.py
+++ bitcoin_networks/map.py
@@ -8,9 +8,11 @@ __version__ = "19.0.1"
 __license__ = ""
 
 #%%
+import pandas as pd
 import geopandas as gpd
 import geoplot as gplt
 import geoplot.crs as gcrs
+from shapely.geometry import Point
 
 
 #%%
@@ -31,12 +33,36 @@ def create_cartogram(filename):
 
 
 #%%
-def points_in_polygon(point_file, poly_file, output_file):
+def csv_to_geodf(filename, lon_col, lat_col):
+    """
+    This function converts CSV with lat/lon data to shapefile
+    """
+    data_raw = pd.read_csv(filename)
+    data_gdf = gpd.GeoDataFrame(
+        data_raw,
+        crs={'init': 'epsg:4326'},
+        geometry=[Point(xy) for xy in zip(data_raw[lon_col], data_raw[lat_col])]
+    )
+    return data_gdf
+
+
+#%%
+def points_in_polygon(point_gdf, poly_gdf):
     """
     This function performs a count points in polygon operation.
+    Thanks to kwinkunks for the initial outline (https://stackoverflow.com/questions/27606924/count-number-of-points-in-multipolygon-shapefile-using-python)
     """
-    pass
-
+    pts = point_gdf.copy()
+    pts_in_polys = []
+    for i, poly in poly_gdf.iterrows():
+        pts_in_this_poly = []
+        for j, pt in pts.iterrows():
+            if poly.geometry.contains(pt.geometry):
+                pts_in_this_poly.append(pt.geometry)
+                pts = pts.drop([j])
+        pts_in_polys.append(len(pts_in_this_poly))
+    poly_gdf['numpoints'] = gpd.GeoSeries(pts_in_polys)
+    return poly_gdf
 
 #%%
 '''
