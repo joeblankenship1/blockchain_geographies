bd35017b15fb70affd8abb1a680200b6894a1221
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-09-23 16:20:23 -0400

    bitcoinwhoswho - added template

---
+++ bitcoin_networks/bitcoinwhoswho.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A library for loading information from BitcoinWhosWho API.
+"""
+
+__author__ = "Joe Blankenship"
+__version__ = "19.0.1"
+__license__ = ""
+
+
+'''
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+'''
